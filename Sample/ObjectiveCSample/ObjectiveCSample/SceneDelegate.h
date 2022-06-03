015b687c9092fbddb85df18405544710ce021463
garimaSemusi <106687726+garimaSemusi@users.noreply.github.com>
2022-06-03 15:11:18 +0530

    Add files via upload

---
+++ Sample/ObjectiveCSample/ObjectiveCSample/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ObjectiveCSample
+//
+//  Created by AIRBOOK on 03/06/22.
+//  Copyright © 2022 AIRBOOK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
